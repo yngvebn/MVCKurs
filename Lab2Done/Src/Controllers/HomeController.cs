c5bc28e1458e2cf68550b1b4da88442a12e2369c
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2012-09-09 19:16:31 +0200

    Lab2Done

---
+++ Lab2Done/Src/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Src.Models;
+
+namespace Src.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel loginViewModel)
+        {
+            return View(loginViewModel);
+        }
+
+        [Authorize]
+        public ActionResult Secret()
+        {
+            return View();
+        }
+    }
+}
