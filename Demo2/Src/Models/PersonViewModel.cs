569ccdc51b17ee3d87fb5b96c4f674178aa82c72
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2012-09-09 19:15:53 +0200

    Del 2
    Lab og demo ferdig

---
+++ Demo2/Src/Models/PersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Src.Models
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+    }
+}
---
+++ Demo2Done/Src/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Src.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name="Brukernavnet ditt her")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password), Display(Name="Skriv inn passordet ditt")]
+        public string Password { get; set; }
+    }
+}
