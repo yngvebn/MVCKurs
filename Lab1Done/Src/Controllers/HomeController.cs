f62da9d043b6ce0a220434039cc42ad0927e21c7
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2012-09-09 18:37:58 +0200

    Del 1
    Slides for Del 1 og Lab for Del 1

---
+++ Lab1Done/Src/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Src.Models;
+
+namespace Src.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            MainViewModel mainViewModel = new MainViewModel()
+                {
+                    Message = "Dette er en beskjed",
+                    Now = DateTime.Now
+                };
+            return View(mainViewModel);
+        }
+
+    }
+}
---
+++ Lab1Done/Src/Models/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Src.Models
+{
+    public class MainViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Now { get; set; }
+    }
+}
